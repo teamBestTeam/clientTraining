964642513ec809a329d9278c5bc0b2987de19142
smb2255 <smb2255@columbia.edu>
2018-02-02 11:47:54 -0500

    set up framework for ui functions, will finish writing them once the handlebars are done and I know what to fill it with

---
+++ assets/scripts/ui.js
@@ -0,0 +1,25 @@
+// need to require the handlebars template
+
+const displayOneBookSuccess =
+}
+
+const displayOneBookFailure = function () {
+
+
+}
+
+const displayManyBooksSuccess = {
+
+}
+
+const displayManyBooksFailure = function () {
+
+
+}
+
+module.exports = {
+  displayOneBookSuccess,
+  displayOneBookFailure,
+  displayManyBooksSuccess,
+  displayManyBooksFailure
+}
