246b2e0931a5d146baa617b947a3abfd99abfeb5
smb2255 <smb2255@columbia.edu>
2018-02-02 13:05:51 -0500

    added the ui and the user messages id in the HTML

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,22 +1,23 @@
+'use strict'
+const displayOneBookTemplate = require('./displayOneBook.handlebars')
+const config = require('./config')
+
 // need to require the handlebars template
 
 const displayOneBookSuccess = displayOneBookTemplate ({book: data.book})
-
+$('#user-messages').html(`<p> Here is your book!</p>`)
 }
 
 const displayOneBookFailure = function () {
 $('#user-messages').html(`<p> Error: no book displayed. </p>`)
-
 }
 
-const displayManyBooksSuccess = {
-
+const displayManyBooksSuccess = displayManyBooksTemplate ({books: data.books})
+$('#user-messages').html(`<p> Here are your books! </p>`)
 }
 
 const displayManyBooksFailure = function () {
 $('#user-messages').html(`<p> Error: no books displayed. </p>`)
-
-
 }
 
 module.exports = {
